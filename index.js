b38bf2628be513435ebf3d997a0f0e99e827f45f
vinnyegon90 <vinnyegon90@gmail.com>
2024-01-16 10:32:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 // Your code here
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+// Process an array of employee data to create records
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+// Create a time-in event for an employee
+function createTimeInEvent(dateTime) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(dateTime.split(" ")[1]),
+        date: dateTime.split(" ")[0]
+    });
+    return this;
+}
+
+// Create a time-out event for an employee
+function createTimeOutEvent(dateTime) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateTime.split(" ")[1]),
+        date: dateTime.split(" ")[0]
+    });
+    return this;
+}
+
+// Calculate hours worked on a specific date
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date).hour;
+    const timeOut = this.timeOutEvents.find(event => event.date === date).hour;
+    return (timeOut - timeIn) / 100;
+}
+
+// Calculate wages earned on a specific date
+function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate.call(this, date);
+    return hours * this.payPerHour;
+}
+
+// Find an employee by first name from a collection of employee records
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName);
+}
+
+// Calculate total payroll for all employees
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, record) => total + allWagesFor.call(record), 0);
+}
+
+// The allWagesFor function is already provided in the code
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
